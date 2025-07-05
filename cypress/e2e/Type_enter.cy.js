b6bdfcfe3ceef936cf124dda0f0b984f0ef8dc11
rubentechQAtro <rubentech4@gmail.com>
2025-07-05 17:20:01 -0500

    Type_enter.cy.js

--- cypress/e2e/Type_enter.cy.js
+++ cypress/e2e/Type_enter.cy.js
@@ -1,42 +1,30 @@
-describe("funcione Type", () => {
-    
-    Cypress.on("uncaught:exception", (err, runnable) => {
-  return false;
+ describe("Función Type - búsqueda en Google", () => {
+  
+  // Ignorar errores de JS del sitio (útil con Google)
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    return false;
+  });
+
+  it("Escribe en el buscador y presiona Enter", () => {
+    // Visita Google
+    cy.visit("https://www.google.com/");
+
+    // Verifica el título
+    cy.title().should("eq", "Google");
+    cy.wait(1500);
+
+    // Escribe en la barra de búsqueda y presiona Enter
+    cy.get("textarea[name='q']")
+      .should("be.visible")
+      .type("cypress testing {enter}");
+
+    // Espera a que cargue la página de resultados
+    cy.wait(2000);
+
+    // Busca un resultado que contenga un texto relacionado
+    cy.contains("Cypress: JavaScript End to End Testing Framework")
+      .should("be.visible")
+      .click();
+  });
+
 });
-
-    
-
-     it ("Types Enter", () => {
-
-        cy.visit("https://www.google.com/")
-        cy.title().should("eq","Google")
-        cy.wait(1500)
-        cy.get("#APjFqb").type("cypres testing {enter}")
-        cy.contains("Cypress: Testing Frameworks for Javascript | Write, Run, Debug").click()
-        
-        
-        
-        
-        //cy.get("#rso > div:nth-child(3) > div > div > div > div.kb0PBd.A9Y9g.jGGQ5e > div > div > span > a > h3").click()
-
-        //cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click()
-       
-        
-        
-        //puedes ingresar una funcion dentro de un string ingresando llaves{} (dentro del las llaves dobñles) esto simular un enter en el teclado
-        
-       
-        // En vez de selectores complejos como #jZ2SBf > .wM6W7d > span, si puedes usar cy.contains('Texto del botón'), será más mantenible.
-
-
-        }) 
-
-
-
-  
-
-    })
-
-    
-
-  
