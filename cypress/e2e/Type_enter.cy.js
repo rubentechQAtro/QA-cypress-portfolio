9c956c64e0225ff4dea1797261e452c884b7a07a
Ruben Rojas <prueba@correo.com>
2025-07-05 17:29:56 -0500

    feat: actualizo archivo Type_enter.cy.js con mejoras

--- cypress/e2e/Type_enter.cy.js
+++ cypress/e2e/Type_enter.cy.js
@@ -15,19 +15,6 @@ describe("funcione Type", () => {
         cy.contains("Cypress: Testing Frameworks for Javascript | Write, Run, Debug").click()
         
         
-        
-        
-        //cy.get("#rso > div:nth-child(3) > div > div > div > div.kb0PBd.A9Y9g.jGGQ5e > div > div > span > a > h3").click()
-
-        //cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click()
-       
-        
-        
-        //puedes ingresar una funcion dentro de un string ingresando llaves{} (dentro del las llaves dobñles) esto simular un enter en el teclado
-        
-       
-        // En vez de selectores complejos como #jZ2SBf > .wM6W7d > span, si puedes usar cy.contains('Texto del botón'), será más mantenible.
-
 
         }) 
 
